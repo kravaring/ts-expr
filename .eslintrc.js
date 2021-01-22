5ba7ae4f6efaf2066569c70fdad800cc20316a11
ragingyngvarr <yngvarr.bel@gmail.com>
2021-01-22 15:48:06 +0300

    Added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+/* eslint-disable */
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    '@typescript-eslint',
+    'prettier',
+  ],
+  'rules': {
+    'prettier/prettier': [
+      1,
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+        semi: false,
+      },
+    ],
+    ...require('eslint-config-prettier').rules,
+    ...require('eslint-config-prettier/@typescript-eslint').rules,
+  },
+};
